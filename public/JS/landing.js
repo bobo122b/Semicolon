544b738443e7c838e4a51844c1d23254a1f45665
Abdelrahman Mostafa <abdelrahman-mostafa@localhost.localdomain>
2023-02-13 22:43:06 +0200

    Adding some cool stuff

--- public/JS/landing.js
+++ public/JS/landing.js
@@ -1,4 +1,6 @@
-let desktopNav = document.getElementById("main-nav__desktop");
+const desktopNav = document.getElementById("main-nav__desktop");
+
+const faders = document.querySelectorAll('.fade-in');
 
 window.onscroll = () => {
     if (document.body.scrollTop >= 450 || document.documentElement.scrollTop >= 450) {
@@ -11,3 +13,24 @@ window.onscroll = () => {
         desktopNav.style.boxShadow = "";
     }
 }
+
+const appearOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -200px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add("appear");
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+})
